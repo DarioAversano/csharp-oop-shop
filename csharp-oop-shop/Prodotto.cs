8a3817a7933ef00bd66da2ed07d4ca45809b2065
darioaversano <risusveloce@gmail.com>
2023-04-18 16:09:22 +0200

    commit 2

---
+++ csharp-oop-shop/Prodotto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    public class Prodotto
+    {
+        // ATTRIBUTI / CARATTESTICHE
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private float prezzo;
+        private float iva;
+
+        // STATI
+        //private bool accesa;
+        //private int numero;
+        //private string stringa;
+
+        // COSTRUTTORE
+        public Prodotto(int codice, string nome, string descrizione, float prezzo, float iva)
+        {
+            this.codice = codice;
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+            //???(?);
+
+            //this.accesa = false;
+        }
+    }
+}
---
+++ csharp-oop-shop/Prodottoproperties.cs
@@ -0,0 +1,27 @@
+﻿namespace csharp_oop_shop
+{
+    public class Prodottoproperties
+    {
+        // ATTRIBUTI con PROPERTIES
+
+        private int codice;
+        public int Codice { get { return this.codice; } }
+
+        private string nome;
+        public string Nome { get { return this.nome; } set { this.nome = value; } }
+
+        private string descrizione;
+        public string Descrizione { get { return this.descrizione; } set { this.descrizione = value; } }
+
+        private float prezzo;
+        public float Prezzo { get { return this.prezzo; } set { this.prezzo = value; } }
+
+        private float iva;
+        public float Iva { get { return this.iva; } set { this.iva = value; } }
+    }
+}
+
+
+
+
+
